95fdd305b8afeab5b1cff29ab42971faa0ebf5b8
Nathan Hamilton <nathan.a.hamilton@gmail.com>
2021-10-12 18:07:08 -0500

    test: true/falase to return

--- palindrome.js
+++ palindrome.js
@@ -2,20 +2,26 @@ const checkPalindrome = (word) => {
 
     // convert string to an array
     const arrayValues = word.split('');
+    // console.log('arrayValues: ', arrayValues);
 
     // reverse the array values
     const reverseArrayValues = arrayValues.reverse();
+    // console.log('reverseArrayValues: ', reverseArrayValues);
 
     // convert array to string
     const reverseWord = reverseArrayValues.join('');
+    // console.log('reverseWord: ', reverseWord);
 
     if (word == reverseWord) {
+        // console.log('true: ', true);
         return true
     }
     else {
+        // console.log('false: ', false);
         return false
     }
 }
+
 checkPalindrome("racecar");
 
 // console.log('test')
